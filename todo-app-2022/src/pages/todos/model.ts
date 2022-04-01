eb49cd5cd33c78f9b9771fdbf4f533e9256166f7
Elvisjan <ilgamilgam@gmail.com>
2022-04-01 22:59:07 +0500

    Добавлены редьюсеры

--- todo-app-2022/src/pages/todos/model.ts
+++ todo-app-2022/src/pages/todos/model.ts
@@ -1,11 +1,28 @@
 import { createEvent, createStore } from "effector";
-
-export const addTodo = createEvent('add todo')
-export const deleteTodo = createEvent('delete todo')
-export const toggleTodo = createEvent('toggle todo')
-
 interface ITodo {
     name: string;
     checked: boolean;
+    id: number;
 }
+
+export const addTodo = createEvent<ITodo>('add todo')
+export const deleteTodo = createEvent<number>('delete todo')
+export const toggleTodo = createEvent<number>('toggle todo')
+
+
 export const $todos = createStore<ITodo[]>([])
+
+$todos.on(addTodo,(state,payload)=>{
+    return [...state,payload]
+})
+$todos.on(deleteTodo,(state,id)=>{
+    return state.filter(t=>t.id !== id)
+})
+$todos.on(toggleTodo,(state,id)=>{    
+    return state.map(t=>{
+        if(t.id===id) {
+            return {...t,checked:!t.checked}
+        }
+        return t
+    })
+})
